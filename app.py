29e5cfead763d4b072c90383ae1a7dee811b5aef
acjin21 <angelacqjin@gmail.com>
2020-02-24 21:15:09 -0500

    import server into index, add secret_key line to app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ links=[
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
+server.secret_key = os.environ.get('secret_key', 'secret')
 app.config.suppress_callback_exceptions = True
