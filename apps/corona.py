eabf4017d296c38433513e0691dd0662edb7cfe2
acjin21 <angelacqjin@gmail.com>
2020-03-02 07:58:03 -0500

    fix date to look at past 29 days due to News API restrictions

--- apps/corona.py
+++ apps/corona.py
@@ -21,9 +21,9 @@ import pandas as pd
 import datetime
 
 today_date=datetime.datetime.today()
-from_date=(today_date-datetime.timedelta(days=30)).date()
+from_date=(today_date-datetime.timedelta(days=29)).date()
 from_date_str=str(from_date)
-
+print(from_date_str)
 url = ('http://newsapi.org/v2/top-headlines?'
        'country=us&'
        'q=Coronavirus&'
